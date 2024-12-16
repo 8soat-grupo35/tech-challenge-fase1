81fb101a7fe209f0fae977c42652d34795529840
gabriel-rosa-da-silva-nunes <gabrielrosadasilvanunes@gmail.com>
2024-12-16 12:24:33 -0300

    feat: add env to project database

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM golang:1.23
 WORKDIR /app
 COPY . /app
 
-RUN go install github.com/air-verse/air@latest
+RUN go install github.com/air-verse/air@v1.61.1
 RUN go install github.com/swaggo/swag/cmd/swag@v1.16.3
 
 COPY go.mod go.sum ./
--- kubernetes/kubernetes_up.sh
+++ kubernetes/kubernetes_up.sh
@@ -1,9 +1,5 @@
 docker build -t fastfood-app:latest .
-kubectl apply -f kubernetes/postgres-pv.yaml
-kubectl apply -f kubernetes/postgres-pvc.yaml
-kubectl apply -f kubernetes/postgres-dbinit-configmap.yaml
-kubectl apply -f kubernetes/postgres-deploy.yaml
-kubectl apply -f kubernetes/postgres-service.yaml
+kubectl apply -f kubernetes/fastfood-secrets.yaml
 kubectl apply -f kubernetes/fastfood-deployment.yaml
 kubectl apply -f kubernetes/fastfood-service.yaml
 kubectl apply -f kubernetes/fastfood-hpa.yaml
